54e12e7038445e8e649950839b2c73c110644bb1
Will Price <will.price94@gmail.com>
2019-02-11 18:55:29 +0000

    Make test.py pytest compatible

--- test.py
+++ test.py
@@ -1,22 +1,46 @@
 import accimage
-image = accimage.Image("chicago.jpg")
-import numpy
-buffer = numpy.empty([image.channels, image.height, image.width], dtype=numpy.uint8)
-#buffer = bytearray(256)
-image.copyto(buffer)
-import scipy.misc
-scipy.misc.imsave('chicago-back.png', numpy.transpose(buffer, (1, 2, 0)))
+import numpy as np
+import imageio
+import os
 
-print(image.size)
+ACCIMAGE_SAVE = os.environ.get('ACCIMAGE_SAVE', '')
+if len(ACCIMAGE_SAVE) and ACCIMAGE_SAVE.lower() not in {'0', 'false', 'no'}:
+    SAVE_IMAGES = True
+else:
+    SAVE_IMAGES = False
 
-image.resize(size=(200, 200))
-buffer = numpy.empty([image.channels, image.height, image.width], dtype=numpy.uint8)
-image.copyto(buffer)
-import scipy.misc
-scipy.misc.imsave('chicago-200x200.png', numpy.transpose(buffer, (1, 2, 0)))
 
-image.crop(box=(50, 50, 150, 150))
-buffer = numpy.empty([image.channels, image.height, image.width], dtype=numpy.uint8)
-image.copyto(buffer)
-import scipy.misc
-scipy.misc.imsave('chicago-100x100.png', numpy.transpose(buffer, (1, 2, 0)))
+def save_image(path, image):
+    image_np = np.empty([image.channels, image.height, image.width], dtype=np.uint8)
+    image.copyto(image_np)
+    image_np = np.transpose(image_np, (1, 2, 0))
+    imageio.imwrite(path, image_np)
+
+
+def test_reading_image():
+    image = accimage.Image("chicago.jpg")
+    assert image.width == 1920
+    assert image.height == 931
+    if SAVE_IMAGES:
+        save_image('test_reading_image.jpg', image)
+
+
+def test_resizing():
+    image = accimage.Image("chicago.jpg")
+
+    image.resize(size=(200, 200))
+
+    assert image.width == 200
+    assert image.height == 200
+    if SAVE_IMAGES:
+        save_image('test_resizing.jpg', image)
+
+def test_cropping():
+    image = accimage.Image("chicago.jpg")
+
+    image.crop(box=(50, 50, 150, 150))
+
+    assert image.width == 100
+    assert image.height == 100
+    if SAVE_IMAGES:
+        save_image('test_cropping.jpg', image)
