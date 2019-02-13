5265dc11fec720840b54bd92d433a04eca80292f
Will Price <will.price94@gmail.com>
2019-02-13 13:50:17 +0000

    Update and tag v0.1.1 release (#15)
    * Correct travis URL to go to travis instead of github in readme
    
    * Bump version to v0.1.1
    
    * Add changelog

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # accimage
 
-[![Build status](https://travis-ci.org/pytorch/accimage.svg?branch=master)](https://github.com/pytorch/accimage)
+[![Build status](https://travis-ci.org/pytorch/accimage.svg?branch=master)](https://travis-ci.org/pytorch/accimage)
 
 An accelerated Image loader and preprocessor leveraging [Intel
 IPP](https://software.intel.com/en-us/intel-ipp).
--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from distutils.core import setup, Extension
 
-accimage = Extension('accimage',
-                    include_dirs = ['/usr/local/opt/jpeg-turbo/include', '/opt/intel/ipp/include'],
-                    libraries = ['jpeg', 'ippi', 'ipps'],
-                    library_dirs = ['/usr/local/opt/jpeg-turbo/lib', '/opt/intel/ipp/lib'],
-                    sources = ['accimagemodule.c', 'jpegloader.c', 'imageops.c'])
+accimage = Extension(
+    "accimage",
+    include_dirs=["/usr/local/opt/jpeg-turbo/include", "/opt/intel/ipp/include"],
+    libraries=["jpeg", "ippi", "ipps"],
+    library_dirs=["/usr/local/opt/jpeg-turbo/lib", "/opt/intel/ipp/lib"],
+    sources=["accimagemodule.c", "jpegloader.c", "imageops.c"],
+)
 
-setup(name = 'accimage',
-      version = '0.1',
-      description = 'Accelerated image loader and preprocessor for Torch',
-      author = 'Marat Dukhan',
-      author_email = 'maratek@gmail.com',
-      ext_modules = [accimage])
+setup(
+    name="accimage",
+    version="0.1.1",
+    description="Accelerated image loader and preprocessor for Torch",
+    author="Marat Dukhan",
+    author_email="maratek@gmail.com",
+    ext_modules=[accimage],
+)
